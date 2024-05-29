95cb81d3aa3dcd32d61452a61961b8aaaf800e7c
zayedutsho <zayedutsho@gmail.com>
2024-05-30 00:10:59 +0600

    banner doner

--- src/comp/Hero.jsx
+++ src/comp/Hero.jsx
@@ -1,12 +1,16 @@
 import logo from "../assets/Sawm Logo.svg";
+import behance from "../assets/icon/behance.svg";
+import dribble from "../assets/icon/dribble.svg";
+import instagram from "../assets/icon/instagram.svg";
+import linkedin from "../assets/icon/link.svg";
 import "../index.css";
 const Hero = () => {
   return (
     <div className="flex flex-col justify-center items-center">
-      <section className="flex justify-center items-center gap-3 mt-[75px] mx-auto">
-        <img src={logo} alt="" className="w-[83px] h-[83px]" />
-        <h1 className="text-4xl font-centra-extrabold italic text-[54px] text-[#1E3A2B]">
-          Swam
+      <section className="flex justify-between items-center gap-3 mt-[75px] ">
+        <img src={logo} alt="" />
+        <h1 className="text-4xl  font-centra-italic-bold text-[54px]  text-[#1E3A2B] italic">
+          Sawm
         </h1>
       </section>
       <div className="text-center py-10">
@@ -23,9 +27,41 @@ const Hero = () => {
           users
         </h2>
 
-        <h2 className="font-centra-normal text-[##52685C] text-[20px] mt-[56px]">
+        <h2 className="font-centra-normal text-[#52685C] mt-[56px] text-[20px]">
           We're on a mission to elevate businesses through designs
         </h2>
+      </div>
+
+      <div className="flex flex-col  md:gap-[24px] sm:flex-row    ">
+        <button className="border rounded-full px-8 py-4 bg-[#BFFF4F] font-centra-extrabold">
+          View Proposal
+        </button>
+        <button className="border-2 border-black rounded-full px-8 py-4 font-centra-extrabold md:mt-0 mt-5">
+          Book a Call
+        </button>
+      </div>
+
+      <div className=" flex flex-row justify-between align-middle gap-[20px] mt-[64px]">
+        <img
+          src={instagram}
+          alt=""
+          className="border bottom-1 border-black  rounded-full p-3"
+        />
+        <img
+          src={dribble}
+          alt=""
+          className="border bottom-1 border-black  rounded-full  p-3"
+        />
+        <img
+          src={behance}
+          alt=""
+          className="border bottom-1 border-black  rounded-full  p-3"
+        />
+        <img
+          src={linkedin}
+          alt=""
+          className="border bottom-1 border-black  rounded-full  p-3"
+        />
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,11 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        "centra-extrabold": ['"Centra No2 TRIAL"', "sans-serif"],
+        "centra-extrabold": ['"Centra No2 TRIAL Bold"', "sans-serif"],
         "centra-italic": ['"Centra No2 TRIAL"', "sans-serif"],
         "dm-serif": ['"DM Serif Display"', "serif"],
         "centra-normal": ['"Centra No2 TRIAL Normal"', "sans-serif"],
+        "centra-italic-bold": ['"Centra No2 TRIAL Italic"', "sans-serif"],
       },
     },
   },
