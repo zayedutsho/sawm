f9a1b5be39b86b4589cd7afbf17a86f8a6de78fd
zayedutsho <zayedutsho@gmail.com>
2024-06-01 21:00:29 +0600

    code fixed

--- src/comp/Client.jsx
+++ src/comp/Client.jsx
@@ -69,17 +69,17 @@ const Client = () => {
           {cards.map((d, index) => (
             <div
               key={index}
-              className="bg-[#264836] rounded-lg text-left mx-auto border-0 px-[50px] md:w-[540px] md:h-[347px]"
+              className="bg-[#264836] rounded-lg text-left mx-auto border-0 px-[38px]  md:px-[60px] md:h-[347px]"
             >
               <img
                 src={d.img}
                 alt=""
-                className="w-16 h-16 mt-3 md:mt-[76px]   "
+                className=" w-[110.16px] h-[18px] mt-[48px] md:w-[171px] md:h-[28px]  mb-[28.99px] md:mt-[76px] md:mb[]   "
               />
-              <p className="font-centra-normal text-white text-[20px] mt-[15px] md:mt-[0px]">
+              <p className="font-centra-normal text-white  text-[16px] md:text-[20px]  mt-[15px] md:mt-[45px]">
                 {d.description}
               </p>
-              <h2 className="text-white text-[24px] font-centra-italic mt-6 md:mt-10 italic">
+              <h2 className="text-white text-[20px] md:text-[24px] font-centra-italic mt-[35px] mb-[35px] md:mt-10 italic">
                 {d.name}
               </h2>
             </div>
